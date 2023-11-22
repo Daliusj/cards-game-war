56a6719caa1b427bc73966d08727443f7c87b015
Dalius Juocas <dalius.juocas@gmail.com>
2023-11-22 16:27:21 +0200

    add_cards() correction

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import random
 
+
 class CardsDeck:
     cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     deck = []
-
 
     @classmethod
     def populate_deck(cls):
@@ -19,7 +19,7 @@ class CardsDeck:
     def split_deck(cls):
         return [cls.deck[:26], cls.deck[26:]]
 
-    
+
 class Player:
     def __init__(self, name):
         self.name = name
@@ -30,7 +30,7 @@ class Player:
         return self.stack[0:num]
 
     def add_cards(self, cards_1, cards_2):
-        self.stack + cards_1 + cards_2
+        self.stack = self.stack + cards_1 + cards_2
 
     def is_winner():
         ...
@@ -44,6 +44,10 @@ def get_players_names():
     name1 = input("Player 1 name: ")
     name2 = input("Player 2 name: ")
     return [name1, name2]
+
+
+def print_table():
+    ...
 
 
 # cards_deck = CardsDeck()
